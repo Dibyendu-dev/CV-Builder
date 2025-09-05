705307ba333d315fdb14ffa1139d89f60646032f
ddas4548 <ddas4548@gmail.com>
2025-09-05 22:46:12 +0530

    routing layer added

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,35 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Layout from './Layout.jsx';
+import { HomePage } from './components/pages/Homepage.jsx';
+import CVpage from './components/pages/CVpage.jsx';
+import Contact from './components/pages/Contactus.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout/>,
+    children: [
+      {
+        path: "",
+        element: <HomePage/>
+      },
+      {
+        path: "createcv",
+        element: <CVpage />
+      },
+      {
+        path: "contact",
+        element: <Contact />
+      }
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+   <RouterProvider router={router} />
   </StrictMode>,
 )
